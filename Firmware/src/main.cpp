ebcc92006a6ab70b4d265da0d511539e62e716a0
Roderick Liao <liaoroderickpersonal@gmail.com>
2026-02-14 14:18:36 -0500

    Test
    Force sensor test

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Set the serial monitor baudrate to 9600
+  Serial.begin(9600);
+  
+  // Output LED Level 1
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int level;
+  level = analogRead(0);
+  
+  // Print "Analog value:" in serial monitor
+  Serial.println("Analog value:");
+  // Print output voltage in serial monitor
+  Serial.println(level);
+  
+  // Turn off all the led initially
+  digitalWrite(13,LOW);
+  
+    // Splitting 1023 into 5 level => 200, 400, 600, 800, 1023
+    // Based on the ADC output, LED indicates the level (1 to 5)
+  
+  if (level>200)
+  {
+    // LEVEL 1 LED
+    digitalWrite(13,HIGH);
+  }
+
+  
 }
